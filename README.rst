80ffe8be2ae5bd863b37244049ab573a2ee7479e
chris-mcdo <cmcdonal33@gmail.com>
2022-05-20 15:31:54 +0100

    initial commit

---
+++ README.rst
@@ -0,0 +1,60 @@
+django-sendmail-backend
+=======================
+
+A simple Django email backend, which uses an installed ``sendmail``-like command to
+dispatch mail.
+
+Based on https://github.com/perenecabuto/django-sendmail-backend.
+
+Features
+--------
+
+Compared to the default SMTP backend, ``django-sendmail-backend``:
+
+- Lets Django get on with other things (rather than waiting for emails to be delivered).
+
+- Integrates better with other system / OS notifications (e.g. error messages from cron.)
+
+- Is easier to debug. (Django may fail silently while sending mail, whereas your
+  local mailserver will at least write to a log.)
+
+Usage
+-----
+
+To use this backend:
+
+1. Install a local mailserver with a ``sendmail``-like command.
+   For example ``msmtp``, ``postfix``, ``DragonFly Mail Agent``, or ``exim``.
+   Configure the mailserver to send mail to your host (the one from the
+   `EMAIL_HOST <https://docs.djangoproject.com/en/dev/ref/settings/#email-host>`_ setting).
+
+2. Install ``django-sendmail-backend`` from pip.
+
+.. code-block:: bash
+
+    $ pip install django-sendmail-backend
+
+  Make sure to use ``django-sendmail-backend`` and not ``django_sendmail_backend``.
+
+3. Add the following to your Django ``settings.py``
+
+.. code-block:: python
+
+    # settings.py
+
+    # The python package is called ``sendmail``
+    EMAIL_BACKEND = "sendmail.SendMailBackend"
+
+    # replace with the path to your ``sendmail``-like command
+    SENDMAIL_COMMAND = "/usr/sbin/sendmail"
+
+That's all!
+
+License
+-------
+
+Copyright (c) 2022 Christopher McDonald
+
+Distributed under the terms of the
+`MIT <https://github.com/chris-mcdo/django-sendmail-backend/blob/main/LICENSE>`_
+license.
---
+++ sendmail/__init__.py
@@ -0,0 +1,65 @@
+"""Email backend which calls sendmail (or another mail command)."""
+import subprocess
+from typing import Iterable
+
+import sysexits
+from django.conf import settings
+from django.core.mail import EmailMessage
+from django.core.mail.backends.base import BaseEmailBackend
+
+
+class SendMailBackend(BaseEmailBackend):
+    """Backend which sends mail via the "sendmail" command."""
+
+    def send_messages(self, email_messages: Iterable[EmailMessage]):
+        """Send multiple emails.
+
+        Parameters
+        ----------
+        email_messages
+            The EmailMessage objects to send.
+
+        Returns
+        -------
+        The number of email messages sent.
+        """
+        sent_count = 0
+        for message in email_messages:
+            if self.send_message(message):
+                sent_count += 1
+
+        return sent_count
+
+    def send_message(self, email_message: EmailMessage):
+        """Send a single email.
+
+        Parameters
+        ----------
+        email_message
+            The EmailMessage object to send.
+        """
+        # Collect recipients
+        recipients = email_message.recipients()
+        if not recipients:
+            return False
+
+        # Collect message
+        message_bytes = email_message.message().as_bytes()
+
+        # Pick sendmail command to use
+        sendmail_command = getattr(settings, "SENDMAIL_COMMAND", "/usr/sbin/sendmail")
+
+        # Attempt mail send
+        process = subprocess.run(
+            [sendmail_command] + recipients, input=message_bytes, stderr=subprocess.PIPE
+        )
+
+        try:
+            sysexits.raise_for_returncode(process)
+        except subprocess.SubprocessError:
+            if self.fail_silently:
+                return False
+            else:
+                raise
+
+        return True
